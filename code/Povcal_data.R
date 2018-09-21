62833ff77d728b805eeff615c0823243ea32a4c3
Alex Miller <alex.k.miller@gmail.com>
2018-09-21 09:44:32 -0400

    Working code

--- code/Povcal_data.R
+++ code/Povcal_data.R
@@ -1,4 +1,4 @@
-list.of.packages <- c("data.table")
+list.of.packages <- c("data.table","plyr")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 lapply(list.of.packages, require, character.only=T)
