45e3ae7b9a67771f031fd594e5b70961ba2760cd
ZChristensen <zach.christensen@devinit.org>
2018-11-09 14:05:11 -0500

    forecasting and current populations in poverty in protracted crisis

--- code/country_share_global_pov.R
+++ code/country_share_global_pov.R
@@ -46,4 +46,37 @@ protracted=protracted[,c("di_id","Poorpop","shareofpoor")]
 so=data.frame(di_id="SO",Poorpop="",shareofpoor="")
 af=data.frame(di_id="AF",Poorpop="",shareofpoor="")
 protracted=rbind(protracted,so,af)
+
+
+forecasts=read.csv("data/WEOpoverty_forecasts.csv")
+forecasts$di_id=NA
+forecasts$di_id[which(forecasts$CountryName=="Syrian Arab Republic")]="SY"
+forecasts$di_id[which(forecasts$CountryName=="Yemen, Republic of")]="YE"
+forecasts$di_id[which(forecasts$CountryName=="West Bank and Gaza")]="PS"
+forecasts$di_id[which(forecasts$CountryName=="South Sudan")]="SS"
+forecasts$di_id[which(forecasts$CountryName=="Sudan")]="SD"
+forecasts$di_id[which(forecasts$CountryName=="Congo, Democratic Republic of")]="CD"
+forecasts$di_id[which(forecasts$CountryName=="Central African Republic")]="CF"
+forecasts$di_id[which(forecasts$CountryName=="Chad")]="TD"
+forecasts$di_id[which(forecasts$CountryName=="Haiti")]="HT"
+forecasts$di_id[which(forecasts$CountryName=="Mali")]="ML"
+forecasts$di_id[which(forecasts$CountryName=="Niger")]="NE"
+forecasts$di_id[which(forecasts$CountryName=="Burkina Faso")]="BF"
+forecasts$di_id[which(forecasts$CountryName=="Mauritania")]="MR"
+forecasts$di_id[which(forecasts$CountryName=="Djibouti")]="DJ"
+forecasts$protracted=1
+forecasts$protracted[which(is.na(forecasts$di_id))]=0
+poorpop2030=data.table(forecasts)[,.(
+  poorpops2030=sum(basepoorpop,na.rm=T)
+),by=c("protracted")]
+
+poorpop2030=data.table(forecasts)[,.(
+  totalpoorpops2030=sum(basepoorpop,na.rm=T)
+)]
+forecasts$global_forecast_poorpops=poorpop2030$totalpoorpops2030
+forecasts$share_forecasted_pov=forecasts$basepoorpop/forecasts$global_forecast_poorpops
+forecasts=forecasts[,c("di_id","share_forecasted_pov","basepoorpop")]
+protracted=join(protracted,forecasts,by=c("di_id"))
+protracted$poorpop2030=protracted$basepoorpop*1000000
+protracted=protracted[,c("di_id","Poorpop","shareofpoor","share_forecasted_pov","poorpop2030")]
 write.csv(protracted,"data/share_of_poor_in_protracted_crises.csv",row.names=F,na="")
